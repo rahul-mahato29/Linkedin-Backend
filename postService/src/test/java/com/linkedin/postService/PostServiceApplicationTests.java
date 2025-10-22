02da48bcedf13ee8af5fe56d3f879fe8f4eb8679
Rahul Mahato <mahatorahul2901@gmail.com>
2025-10-22 21:42:33 +0530

    initial commit

---
+++ postService/src/test/java/com/linkedin/postService/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.postService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ userService/src/test/java/com/linkedin/userService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
